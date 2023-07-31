ff6950358042b3b14fab93bb6114b159ddcaa790
Rahul <rahulsmrd@gmail.com>
2023-07-31 20:49:38 +0530

    changed

--- todolist/settings.py
+++ todolist/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo',
+        'USER': 'root',
+        'PASSWORD': 'Rahul@2003',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
